06952589b62eb7a855cfd3dd3c45120f13d58a09
Natsec <kamilp@hotmail.fr>
2022-03-11 19:56:02 +0100

    typo

--- dotfiles/dotfiles.sh
+++ dotfiles/dotfiles.sh
@@ -25,4 +25,4 @@ get /~/.config/xfce4/terminal/terminalrc
 # Paquets
 # --------------------
 sudo apt update
-sudo apt install htop tmux tree ncdu neofetch & clear & neofetch
+sudo apt install htop tmux tree ncdu neofetch && clear && neofetch
